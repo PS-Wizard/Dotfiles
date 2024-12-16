b647dc581eefedce7a03dc9640f1277bad3fa86c
PS-Wizard <p.swoyam.1@gmail.com>
2024-12-16 15:49:36 +0545

    added quick fix list thingy

--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -1,10 +1,27 @@
 local opts = { noremap = true, silent = true }
 
+-- Navigate Quickfix list
+vim.api.nvim_set_keymap('n', '<A-k>', ':cnext<CR>', opts) -- Alt+l to go down
+vim.api.nvim_set_keymap('n', '<A-j>', ':cprev<CR>', opts) -- Alt+h to go up
+
+-- Add current line to Quickfix list
+vim.api.nvim_set_keymap(
+  'n',
+  '<leader>a',
+  ':lua vim.fn.setqflist({{filename = vim.fn.expand("%"), lnum = vim.fn.line("."), col = vim.fn.col("."), text = vim.fn.getline(vim.fn.line("."))}}, "a")<CR>',
+  opts
+)
+
+-- Clear Quickfix list
+vim.api.nvim_set_keymap('n', '<leader>cc', ':lua vim.fn.setqflist({})<CR>', opts) -- Clear Quickfix list
+
 -- Toggle between the latest buffers
 vim.api.nvim_set_keymap('n', '<leader>n', '<C-^>', opts)
 
--- Mapping for <leader>ab to insert a code block template
-vim.api.nvim_set_keymap('n', '<leader>ab', 'i```<CR>~<CR><CR>```<Esc>O', { noremap = true, silent = true })
+-- Insert a code block template
+vim.api.nvim_set_keymap('n', '<leader>ab', 'i```<CR>~<CR><CR>```<Esc>O', opts)
 
--- Mapping for <leader>ab to insert a math inline-block template
-vim.api.nvim_set_keymap('n', '<leader>am', 'i$$<Esc>i', { noremap = true, silent = true })
+
+-- Insert a math inline-block template
+vim.api.nvim_set_keymap('n', '<leader>am', 'i$$<Esc>i', opts)
+
