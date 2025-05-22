b6ac50a29bcd6912640344df9303a6e0ff51d3ec
Wizard <p.swoyam.1@gmail.com>
2025-05-22 15:40:09 +0545

    updated

--- nix/config.nix
+++ nix/config.nix
@@ -91,3 +91,62 @@ in {
     system.stateVersion = "23.11";
 }
 
+
+{ config, pkgs, ... }:
+
+
+################# 
+
+let
+  unstable = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") {
+    config.allowUnfree = true;
+  };
+in {
+  imports = [ ./hardware-configuration.nix ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  nixpkgs.config.allowUnfree = true;
+
+  services.xserver.enable = true;
+  services.xserver.windowManager.i3.enable = true;
+  services.xserver.displayManager.startx.enable = true;
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = true;
+    powerManagement.finegrained = false;
+    open = false;
+    nvidiaSettings = true;
+
+    prime = {
+      offload.enable = true;
+      amdBusId = "PCI:100:0:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    glxinfo
+    alacritty
+    neovim
+    xorg.xinit        # gives you startx
+    xterm             # fallback terminal if alacritty fails
+    dmenu             # needed by default i3 config
+    unstable.zen-browser
+    nvidia-smi
+    nvidia-settings
+  ];
+
+  users.users.wizard = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "video" ];
+    shell = pkgs.zsh; # or bash/fish if you prefer
+  };
+
+  system.stateVersion = "23.11";
+}
+
